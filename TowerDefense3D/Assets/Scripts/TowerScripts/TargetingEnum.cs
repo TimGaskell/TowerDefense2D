447ef9408b1b1815ac7302e38d3e73e5742a46a8
Tim <tgaskell23@gmail.com>
2020-06-04 14:35:08 +1000

    Start

---
+++ TowerDefense3D/Assets/Scripts/TowerScripts/TargetingEnum.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Targeting
+{
+    First,
+    Last,
+    Closest,
+    Strongest
+}
---
+++ TowerDefense3D/Assets/Scripts/TowerScripts/TowerBase.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TowerBase : MonoBehaviour
+{
+
+    [SerializeField]
+    protected float AttackDamage, AttackSpeed, AttackRange;
+
+    [SerializeField]
+    protected Targeting targetingPriority;
+
+    [SerializeField]
+    protected GameObject LaunchObject;
+
+    private List<GameObject> EnemiesInRange;
+
+
+    private void Update() {
+        
+        //If something is within Attack Range
+            //If target fits targeting priority 
+                //If can fire again          
+                    //Attack
+
+    }
+
+
+    public virtual void Attack() {
+
+    }
+
+    GameObject FindEnemyToAttack() {
+
+        return null;
+    }
+  
+
+    float CheckDistance() {
+
+
+        return 1;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+
+        if (other.GetComponent<>) {
+            EnemiesInRange.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.GetComponent<>) {
+            EnemiesInRange.Remove(other.gameObject);
+        }
+    }
+}
